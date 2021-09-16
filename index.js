98703bd71f89473175a70f66d7b4fe6258e131a0
Krisha <krisha.magar77@gmail.com>
2021-09-16 10:22:20 +0545

    2021/9/16 new commit.

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ console.log(example);
 
 // undefined datatype
 let iims;
+
 console.log(iims);
 let person = {
     name: 'Diane',
@@ -33,3 +34,64 @@ let exampleArray = Array(1,2,3,3,4,5,6,6,8,9,10);
 let exampleArray2 = [1,2,3,4,6,7,8,9,10]
 console.log(exampleArray);
 console.log(exampleArray2);
+
+let firstLetter = "Hello";
+var secondLetter = "Wrld";
+let thirdLetter = firstLetter + secondLetter;
+console.log("Lenght of the third letter is: ", thirdLetter.length);
+
+//using slice which lets us to have the particular part of the sentence or word.
+let note = "I am from Hetauda";
+console.log(note.slice(10, note.length));
+
+let firstWord = "Hello ";
+let secWord = "World";
+let replaceWord = "IIMS";
+let concatWord = firstWord.concat( secWord);
+console.log(concatWord.replace ("World", replaceWord));
+
+//change to lower case
+let word = "HELLO KRISHA";
+console.log(word.toLowerCase());
+//chnage to upper case
+let word1 = "hello universe";
+console.log(word1.toUpperCase());
+
+// trim a string and get length
+let example4 = "   Hello World   ";
+console.log("Length of example4: ",example4.length);
+example4 = example4.trim();
+console.log("Trimmed value: ", example4);
+console.log("Length of example4: ",example4.length);
+// toString function
+let num = 22;
+console.log("First:", typeof num);
+num = num.toString();
+console.log("Last:", typeof num);
+
+// get substr
+let str = "Hello world";
+let result = str.substr(0 , 6);
+console.log(result);
+
+
+
+//get index of
+let checkIndexOf = "Hi there";
+console.log(checkIndexOf.indexOf("there"));
+
+//includes of
+let checkIncludes = "hello there";
+console.log("check if checkIncludes includes there", checkIncludes.includes("there"));
+console.log("check if checkIncludes includes world", checkIncludes.includes("world"));
+
+//check char at
+let checkCharAt = "I am from Hetauda";
+console.log(checkCharAt.length);
+console.log(checkCharAt.charAt(2));
+
+let padStartExample = "Hello";
+console.log(padStartExample.padStart(13, "IIMS"));
+
+let padEndExample = "Hello";
+console.log(padEndExample.padEnd(13, "IIMS"));
