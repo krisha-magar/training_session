59a1f57f423e8198b3b52af3ba6c8891185963ef
Krisha <krisha.magar77@gmail.com>
2021-09-19 08:33:55 +0545

    types of operators.

---
+++ operators.js
@@ -0,0 +1,19 @@
+//Operators
+let a = 4;
+let b = 5;
+//a += a;
+a += b;
+console.log("The sum of a and b is", a);
+
+// to check the datatype three assignment symbol is checked.
+if (1 === "1") {
+    console.log("This statement is true");
+}
+
+if (1 == "1") {
+    console.log("This statement is true");
+} else {
+    console.log("This Statement is false.");
+}
+
+console.log(1 === "1" ? "this statement is true" : "this statement is false");
